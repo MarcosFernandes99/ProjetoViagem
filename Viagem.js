d8d8b56639e76fea72d56edcdcd25896d254c375
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-09-28 23:12:29 -0300

    Atualizando projeto com as funções incrementando a lógica para o usuário cladastar-cliente-voo-pacoteViagem

--- Viagem.js
+++ Viagem.js
@@ -1,3 +1,7 @@
+let clientes = []
+let voos = []
+let pacotesViagem = []
+
 class Cliente {
     #Nome
     #Cpf
@@ -200,7 +204,7 @@ class PassagemAerea {
             this.#Valor = this.CalcularAcrescimo(valor)
             return
         }
-        else{
+        else {
             console.log(`ERRO! Valor menor que 0`)
         }
 
@@ -213,14 +217,70 @@ class PassagemAerea {
     }
 }
 
-let pessoa = new Cliente("Marcos", "44455544412", "21/10/1994")
-console.log(pessoa)
+let continuar = true
+do{
+let opcao = prompt(`Digite 1-Cadastrar Cliente/2-Cadastrar Voo/3-Cadastrar Pacote de Viagem`)
 
-let viagemUm = new PacoteViagem(pessoa, "14/10/2022", "25/10/2022", 3000)
+switch (opcao) {
+    case "1":
+        CadastrarCliente()
+        break;
+    case "2":
+        CadastrarVoo()
+        break;
+    case "3":
+        CadastrarPacoteViagem()
+        break;
+}
 
-let vooUm = new Voo("Gol", "255", "14/10/2022", "12:30", "Congonhas", "Salvador")
+let opcaoContinuar = prompt(`1-Continuar/2-Encerrar`)
+if(opcaoContinuar != 1){
+    continuar = false
+}
+}while(continuar)
 
-let passagem = new PassagemAerea("455", true, 50, pessoa, vooUm)
+function CadastrarCliente() {
+    // let nomeAd = prompt(`Qual o seu nome?`)
+    // let cpfAd = prompt(`Qual o seu CPF?`)
+    // let dataNascimentoAd = prompt(`Qual a sua data de nascimento?`)
 
-passagem.ExibirResumo()
+    let novoCliente = new Cliente("Marcos", "45555566678", "21/10/2022")
+    clientes.push(novoCliente)
+    console.log(clientes)
+}
 
+function CadastrarVoo() {
+    // let empresa = prompt(`Qual a empresa aérea?`)
+    // let numero = prompt(`Qual o número do Voo?`)
+    // let data = prompt(`Qual a data do Voo?`)
+    // let horario = prompt(`Qual o horário do Voo?`)
+    // let localPartida = prompt(`Qual o local de partida?`)
+    // let localDestino = prompt(`Qual o local de destino?`)
+
+    let vooUm = new Voo("Gol", "255", "14/10/2022", "12:30", "São Paulo", "Salvador")
+    voos.push(vooUm)
+    let vooDois = new Voo("Latam", "123", "20/10/2022", "16:00", "São Paulo", "Rio De Janeiro")
+    voos.push(vooDois)
+    let vooTrês = new Voo("Latam", "456", "05/10/2022", "20:00", "São Paulo", "Brasília")
+    voos.push(vooTrês)
+    console.log(voos)
+}
+
+function CadastrarPacoteViagem() {
+    let titular = prompt(`Qual o titular do pacote?`)
+    voos.forEach(x => {
+        console.log(x)
+    })
+    let passagemIda = prompt(`Qual a data da passagem de IDA?`)
+    let passagemVolta = prompt(`Qual a data da passagem de VOLTA?`)
+    let valorTotal = prompt(`Qual o valor total?`)
+
+    let novoPacote = new PacoteViagem(titular, passagemIda, passagemVolta, valorTotal)
+    //let novoPacote = new PacoteViagem("Marcos", "05/10/2022", "15/10/2022", 3000)
+    pacotesViagem.push(novoPacote)
+    console.log(pacotesViagem)
+}
+
+//let viagemUm = new PacoteViagem(pessoa, "14/10/2022", "25/10/2022", 3000)
+//let passagem = new PassagemAerea("455", true, 50, pessoa, vooUm)
+//passagem.ExibirResumo()
